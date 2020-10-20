e4d452e697324cdaa4359fda764327683923b244
cfierke <46982767+cfierke@users.noreply.github.com>
2020-10-20 14:45:20 -0500

    Tutorial completed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useEffect, useState} from 'react';
 import './App.css';
+import Recipe from './Recipe';
 
 const App = () => {
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('chicken')
 
   const APP_ID = '13061f51';
   const APP_KEY = 'e5e4c976e417f757b1e6373bb56e3c74';
 
-  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  useEffect(() => {
+    getRecipes();
+  }, [query]);
+
+  const getRecipes = async() => {
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`)
+    const data = await response.json();
+    setRecipes(data.hits);
+    console.log(data.hits);
+  }
+
+  const UpdateSearch = e => {
+    setSearch(e.target.value);
+    console.log(search);
+  }
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch('')
+  }
 
   return(
     <div className="App">
-      <form className="search-form">
-        <input className="search-bar" type="text"/>
-        <button className="search-button" type="submit">Search</button>
+      <form onSubmit={getSearch} className="search-form">
+        <input
+          className="search-bar"
+          type="text"
+          value={search}
+          onChange={UpdateSearch}
+        />
+        <button
+          className="search-button"
+          type="submit">Submit
+        </button>
       </form>
+      <div className="recipes">
+        {recipes.map(recipe =>(
+          <Recipe
+            key={recipe.recipe.label}
+            title={recipe.recipe.label}
+            calories={Math.round(recipe.recipe.calories)}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+          />
+        ))}
+      </div>
     </div>
   );
 }
